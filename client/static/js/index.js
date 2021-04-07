62235e401a241df07e0478d6268e85759f789d85
mahessh77melo <magesh.lbj23@gmail.com>
2021-04-07 11:36:27 +0530

    overlay forms

--- client/static/js/index.js
+++ client/static/js/index.js
@@ -1,5 +1,19 @@
-// Leaflet map inclusion
+// DOM Elements
+const landerSection = document.querySelector(".section__lander");
+const featureSection = document.querySelector(".section__features");
+const mapSection = document.querySelector(".section__map");
+const chartSection = document.querySelector(".section__chart");
+const footerSection = document.querySelector(".section__footer");
+const featureNavBtn = document.querySelector(".nav__item#features");
+const mapNavBtn = document.querySelector(".nav__item#map");
+const chartNavBtn = document.querySelector(".nav__item#chart");
+const homeFooterBtn = document.querySelector(".footer__link#home");
+const loginBtn = document.querySelector(".btn-login");
+const registerBtn = document.querySelector(".footer__link#register");
+const loginOverlay = document.querySelector("#login-overlay");
+const registerOverlay = document.querySelector("#register-overlay");
 
+// Leaflet map inclusion
 const initMap = function () {
 	// function to display the map
 	const showMap = function ({ coords }) {
@@ -70,6 +84,50 @@ const initChart = function () {
 		},
 	});
 };
-initChart();
-// calling the function that manages the map
-initMap();
+
+const initNavigators = function () {
+	const addNavigator = function (btn, targetSection) {
+		btn.addEventListener("click", () => {
+			targetSection.scrollIntoView();
+		});
+	};
+	addNavigator(featureNavBtn, featureSection);
+	addNavigator(mapNavBtn, mapSection);
+	addNavigator(chartNavBtn, chartSection);
+	addNavigator(homeFooterBtn, landerSection);
+};
+
+const initOverlays = function () {
+	// to display
+	loginBtn.addEventListener("click", () => {
+		loginOverlay.style.display = "flex";
+	});
+	registerBtn.addEventListener("click", () => {
+		registerOverlay.style.display = "flex";
+	});
+
+	// to hide
+	const hideBoth = function () {
+		[loginOverlay, registerOverlay].forEach((o) => (o.style.display = "none"));
+	};
+	// close button is clicked
+	const closeButtons = [...document.querySelectorAll(".close-button")];
+	closeButtons.forEach((btn) => btn.addEventListener("click", hideBoth));
+	// overlay is clicked
+	const overlays = [...document.querySelectorAll(".overlay")];
+	overlays.forEach((overlay) => overlay.addEventListener("click", hideBoth));
+	// escape key is pressed
+	window.addEventListener("keyup", (e) => {
+		if (e.key == "Escape") {
+			hideBoth();
+		}
+	});
+};
+
+const init = function () {
+	initChart();
+	initMap();
+	initNavigators();
+	initOverlays();
+};
+init();
